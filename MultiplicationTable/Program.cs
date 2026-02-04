ccdf73f9f8f71570a87e7f6c0c4ba3294eaadf31
Hunter Clezie <clezhunt@gmail.com>
2026-02-04 16:26:55 -0700

    add custom and default option for multiplication table

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -21,14 +21,22 @@
                 try
                 {
                     // risky code
-                    result = int.Parse(userInput);
-                    isValid = true;
+                    for (int row = 1; row < int.Parse(userInput) + 1; row++)
+                    {
+                        for (int column = 1; column < int.Parse(userInput) + 1; column++)
+                        {
+                            Console.Write((row * column).ToString().PadLeft(5));
+                        }
+                        Console.WriteLine();
+                        isValid = true;
+                    }
 
                 }
                 catch (Exception ex)
                 {
                     if (userInput == "D" || userInput == "d")
                     {
+                        Console.WriteLine($"You've selected the 12x12 multiplication table!");
                         for (int row = 1; row < 12 + 1; row++)
                         {
                             for (int column = 1; column < 12 + 1; column++)
@@ -42,6 +50,7 @@
 
                     else
                     {
+                        Console.WriteLine($"You've selected the {userInput}x{userInput} multiplication table!");
                         for (int row = 1; row < int.Parse(userInput) + 1; row++)
                         {
                             for (int column = 1; column < int.Parse(userInput) + 1; column++)
@@ -49,6 +58,7 @@
                                 Console.Write((row * column).ToString().PadLeft(5));
                             }
                             Console.WriteLine();
+                            isValid = true;
                         }
                     }
                 }
@@ -56,25 +66,25 @@
             }
             while (isValid == false);
 
-            //Console.WriteLine("please enter your first number");
-            //userInput = Console.ReadLine();
-            //firstNumber = int.Parse(userInput);
-            //Console.WriteLine($"You entered {firstNumber}");
+                //Console.WriteLine("please enter your first number");
+                //userInput = Console.ReadLine();
+                //firstNumber = int.Parse(userInput);
+                //Console.WriteLine($"You entered {firstNumber}");
 
-            //Console.WriteLine("please enter your second number");
-            //userInput = Console.ReadLine();
-            //secondNumber = int.Parse(userInput);
-            //Console.WriteLine($"You entered {secondNumber}");
+                //Console.WriteLine("please enter your second number");
+                //userInput = Console.ReadLine();
+                //secondNumber = int.Parse(userInput);
+                //Console.WriteLine($"You entered {secondNumber}");
 
-            //{
-            //    for (int row = 1; row < firstNumber + 1; row++)
-            //    {
-            //        for (int column = 1; column < secondNumber + 1; column++)
-            //        {
-            //            Console.Write((row * column).ToString().PadLeft(5));
-            //        }
-            //        Console.WriteLine();
-            //    }
+                //{
+                //    for (int row = 1; row < firstNumber + 1; row++)
+                //    {
+                //        for (int column = 1; column < secondNumber + 1; column++)
+                //        {
+                //            Console.Write((row * column).ToString().PadLeft(5));
+                //        }
+                //        Console.WriteLine();
+                //    }
                 //pause
                 Console.Read();
             }
