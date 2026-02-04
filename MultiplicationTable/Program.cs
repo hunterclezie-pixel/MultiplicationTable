4f8c76fdbdcb7e3908cc11774d7b0e6ed6eac545
Hunter Clezie <clezhunt@gmail.com>
2026-02-03 18:48:00 -0700

    Add way to pick multiplication table

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput = "";
+            int firstNumber, secondNumber;
+
+            Console.WriteLine("please enter your first number");
+            userInput = Console.ReadLine();
+            firstNumber = int.Parse(userInput);
+            Console.WriteLine($"You entered {firstNumber}");
+
+            Console.WriteLine("please enter your second number");
+            userInput = Console.ReadLine();
+            secondNumber = int.Parse(userInput);
+            Console.WriteLine($"You entered {secondNumber}");
+
+            {
+                for (int row = 1; row < firstNumber + 1; row++)
+                {
+                    for (int column = 1; column < secondNumber + 1; column++)
+                    {
+                        Console.Write((row * column).ToString().PadLeft(5));
+                    }
+                    Console.WriteLine();
+                }
+                //pause
+                Console.Read();
+            }
         }
     }
 }
